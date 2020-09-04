981f8071c62f011f3227fcd0dfb1565e37c6600b
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-04 12:12:37 -0700

    env file set up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
+require('dotenv').config()
 const express = require('express') 
+const massive = reqire('massive')
 const controller = require('./controller')
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env
 
 const app = express()
 
 app.use(express.json())
+
+// const baseUrl = ''
+// app.get()
+// app.post()
+// app.put() <-- not listed in plans
+// app.delete() <-- not listed in plans
 
 const port = 4000
 app.listen(port, () => {
